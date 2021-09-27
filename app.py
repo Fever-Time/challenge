8bcdfffcb28b6c3be6fcfbc1a9c0aafb9d8f748b
gusrb8925 <gusrb8925@naver.com>
2021-09-27 16:39:31 +0900

    수정 브런치 병합결과

--- app.py
+++ app.py
@@ -2,43 +2,41 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-
 from pymongo import MongoClient
-
-from bson.json_util import loads, dumps
+from bson.objectid import ObjectId
 
 from datetime import datetime
-
-
 
 client = MongoClient('mongodb://test:test@13.124.151.213:27017')
 db = client.ftime
 
 
 @app.route('/', methods=['GET'])
-def index():
-
-     return render_template('index.html')
+def main_page():
+    challenges = objectIdDecoder(list(db.challenge.find({})))
+    return render_template('index.html', challenges=challenges)
 
 
 @app.route('/error', methods=['GET'])
-def error():
+def error_page():
     return render_template('404.html')
 
 
 @app.route('/user', methods=['GET'])
-def user():
+def user_detail_page():
     return render_template('user.html')
 
 
 @app.route('/challenge/create', methods=['GET'])
-def challenge_create():
+def challenge_create_page():
     return render_template('challenge-create.html')
 
 
-@app.route('/challenge', methods=['GET'])
-def challenge_detail():
-    return render_template('challenge-detail.html')
+@app.route('/challenge/<challengeId>', methods=['GET'])
+def challenge_detail_page(challengeId):
+    challenge = db.challenge.find_one({'_id': ObjectId(challengeId)})
+    challenge['_id'] = str(challenge['_id'])
+    return render_template("challenge-detail.html", challenge=challenge)
 
 
 # 준호님 code start
@@ -48,13 +46,12 @@ def challenge_detail():
 
 # 수빈님 code start
 
-@app.route('/cc', methods=['GET'])
-def show_challenge():
-    challenges = list(db.challenge.find({}))
-    json_str = dumps(challenges)
-    print(json_str)
-    return jsonify({'all_challenges': json_str})
-
+# @app.route('/challenge', methods=['GET'])
+# def get_challenge():
+#     challenges = list(db.challenge.find({}))
+#     json_str = dumps(challenges)
+#     print(json_str)
+#     return jsonify({'all_challenges': json_str})
 
 
 @app.route('/challenge', methods=['POST'])
@@ -67,7 +64,7 @@ def save_challenge():
     # file_len 이 0이면 JS에서 파일을 안보낸준 것!
     # 파일을 안보내줬으면 default 파일이름을 넘겨준다.
     if file_len == 0:
-        full_file_name = "default.png" # default 파일이름 설정
+        full_file_name = "default.png"  # default 파일이름 설정
     else:
         # 파일을 제대로 전달해줬으면 파일을 꺼내서 저장하고 파일이름을 넘겨준다.
         image_receive = request.files["image_give"]
@@ -93,29 +90,25 @@ def save_challenge():
     }
 
     db.challenge.insert_one(doc)
-    return jsonify({'msg': "챌린지 등록 되었습니다." })
-
-@app.route('/detail/<challengeId>')
-def detail(challengeId):
-
-    return render_template("challenge-detail.html", challengeId=challengeId)
-
+    return jsonify({'msg': "챌린지 등록 되었습니다."})
 
 
 @app.route('/challenge/delete', methods=['POST'])
 def delete_word():
-    # 단어 삭제하기
     host_receive = request.form['host_give']
-    db.challenge.delete_one({'challenge_host' : host_receive})
-
+    db.challenge.delete_one({'challenge_host': host_receive})
     return jsonify({'result': 'success', 'msg': '챌린지 삭제 되었습니다.'})
-
-
 
 
 # 수빈님 code end
 
 # 현규님 code start
+def objectIdDecoder(list):
+    results = []
+    for document in list:
+        document['_id'] = str(document['_id'])
+        results.append(document)
+    return results
 
 
 # 현규님 code end
