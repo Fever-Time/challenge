fa8ed4534d84960d5d6b7b1273b88d6e42345314
suubinkim <0vbbbv0@gmail.com>
2021-09-27 21:23:04 +0900

    추가 챌린지 인증 (#10)

--- app.py
+++ app.py
@@ -2,14 +2,11 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-
 from pymongo import MongoClient
-
+from bson.objectid import ObjectId
 from bson.json_util import loads, dumps
 
 from datetime import datetime
-
-
 
 client = MongoClient('mongodb://test:test@13.124.151.213:27017')
 db = client.ftime
@@ -17,8 +14,7 @@ db = client.ftime
 
 @app.route('/', methods=['GET'])
 def index():
-
-     return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route('/error', methods=['GET'])
@@ -51,10 +47,10 @@ def challenge_detail():
 @app.route('/cc', methods=['GET'])
 def show_challenge():
     challenges = list(db.challenge.find({}))
+    print("list", challenges)
     json_str = dumps(challenges)
     print(json_str)
     return jsonify({'all_challenges': json_str})
-
 
 
 @app.route('/challenge', methods=['POST'])
@@ -67,7 +63,7 @@ def save_challenge():
     # file_len 이 0이면 JS에서 파일을 안보낸준 것!
     # 파일을 안보내줬으면 default 파일이름을 넘겨준다.
     if file_len == 0:
-        full_file_name = "default.png" # default 파일이름 설정
+        full_file_name = "default.png"  # default 파일이름 설정
     else:
         # 파일을 제대로 전달해줬으면 파일을 꺼내서 저장하고 파일이름을 넘겨준다.
         image_receive = request.files["image_give"]
@@ -93,25 +89,59 @@ def save_challenge():
     }
 
     db.challenge.insert_one(doc)
-    return jsonify({'msg': "챌린지 등록 되었습니다." })
+    return jsonify({'msg': "챌린지 등록 되었습니다."})
 
-@app.route('/detail/<challengeId>')
+
+@app.route('/challenge/<challengeId>')
 def detail(challengeId):
-
-    return render_template("challenge-detail.html", challengeId=challengeId)
-
+    challenge = db.challenge.find_one({'_id': ObjectId(challengeId)})
+    print(challenge)
+    return render_template("challenge-detail.html", challengeId=challengeId, challenge=challenge)
 
 
 @app.route('/challenge/delete', methods=['POST'])
 def delete_word():
     # 단어 삭제하기
     host_receive = request.form['host_give']
-    db.challenge.delete_one({'challenge_host' : host_receive})
+    db.challenge.delete_one({'challenge_host': host_receive})
 
     return jsonify({'result': 'success', 'msg': '챌린지 삭제 되었습니다.'})
 
+@app.route('/challenge/check', methods=['POST'])
+def challenge_check():
+    challenge_receive = request.form["challenge_give"]
+    cont_receive = request.form["cont_give"]
+    file = request.files["img_give"]
+
+    extension = file.filename.split('.')[-1]
+
+    today = datetime.now()
+    mytime = today.strftime("%Y-%m-%d-%H-%M-%S")
+    uploadtime = today.strftime("%Y-%m-%d")
+
+    filename = f'file-{mytime}'
+
+    save_to = f'static/assets/img/{filename}.{extension}'
+    file.save(save_to)
+
+    doc = {
+        'join_challenge': challenge_receive,
+        'join_date': uploadtime,
+        'join_cont': cont_receive,
+        'join_img' : f'{filename}.{extension}',
 
 
+    }
+
+    db.join.insert_one(doc)
+    return jsonify({'msg': "챌린지 인증 되었습니다."})
+
+@app.route('/challenge/get', methods=['GET'])
+def challenge_get():
+    challenge_receive = request.args.get('challenge_give')
+    challenges = list(db.join.find({'join_challenge': challenge_receive}, {"_id": False}))
+    # 예문 가져오기
+    return jsonify({'all_challenges': challenges})
 
 # 수빈님 code end
 
