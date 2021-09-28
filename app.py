d8d33b26590d56e75b012418fd0be247207112f7
suubinkim <0vbbbv0@gmail.com>
2021-09-28 16:54:38 +0900

    추가 생성 유효성 검사 (#20)

--- app.py
+++ app.py
@@ -198,15 +198,16 @@ def check_dup():
 @app.route('/challenge', methods=['POST'])
 def save_challenge():
     title_receive = request.form["title_give"]
-    decs_receive = request.form["desc_give"]
+    desc_receive = request.form["desc_give"]
     period_receive = request.form["period_give"]
     address_receive = request.form["address_give"]
+    # host_receive = request.form["host_give"]
 
     file_len = len(request.files)
     # file_len 이 0이면 JS에서 파일을 안보낸준 것!
     # 파일을 안보내줬으면 default 파일이름을 넘겨준다.
     if file_len == 0:
-        full_file_name = "default.png"  # default 파일이름 설정
+        full_file_name = "challenge.jfif"  # default 파일이름 설정
     else:
         # 파일을 제대로 전달해줬으면 파일을 꺼내서 저장하고 파일이름을 넘겨준다.
         image_receive = request.files["image_give"]
@@ -217,19 +218,19 @@ def save_challenge():
 
         filename = f'file-{mytime}'
 
-        save_to = f'static/assets/img/{filename}.{extension}'
+        save_to = f'static/assets/img/challenge/{filename}.{extension}'
         image_receive.save(save_to)
 
         full_file_name = f'{filename}.{extension}'
 
     doc = {
         'challenge_title': title_receive,
-        'challenge_desc': decs_receive,
+        'challenge_desc': desc_receive,
         'challenge_img': full_file_name,
         'challenge_startTime': period_receive.split(',')[0],
         'challenge_endTime': period_receive.split(',')[1],
         'challenge_address': address_receive,
-        'challenge_host': 'admin'
+        'challenge_host':  'admin'
     }
 
     db.challenge.insert_one(doc)
@@ -257,7 +258,7 @@ def challenge_check():
 
     filename = f'file-{mytime}'
 
-    save_to = f'static/assets/img/{filename}.{extension}'
+    save_to = f'static/assets/img/join/{filename}.{extension}'
     file.save(save_to)
 
     doc = {
