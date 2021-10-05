8aa264386a3066079b64daf8832822cd199e7f26
JunHo KANG <junho-yh>
2021-10-05 10:50:14 +0900

    코드 리팩토링 (#36)

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ def main_page():
     challenges = objectIdDecoder(list(db.challenge.find({})))
     for challenge in challenges:
         challenge['people'] = len(list(db.join.distinct("join_user", {"join_challenge": challenge['_id']})))
-
     return render_template('index.html', challenges=challenges)
 
 
@@ -43,7 +42,7 @@ def user():
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 
-@app.route('/challenge/create', methods=['GET'])
+@app.route('/challenge', methods=['GET'])
 def challenge_create_page():
     return render_template('challenge-create.html')
 
@@ -102,20 +101,6 @@ def login():
     return render_template('challenge-login.html', msg=msg)
 
 
-# @app.route('/user/<username>')
-# def user(username):
-#     # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
-#
-#         user_info = db.users.find_one({"username": username}, {"_id": False})
-#         return render_template('user.html', user_info=user_info, status=status)
-#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-#         return redirect(url_for("home"))
-#
-
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
     # 로그인
@@ -145,10 +130,6 @@ def sign_up():
     doc = {
         "username": username_receive,  # 아이디
         "password": password_hash,  # 비밀번호
-        # "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
-        # "profile_pic": "",                                          # 프로필 사진 파일 이름
-        # "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
-        # "profile_info": ""                                          # 프로필 한 마디
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
@@ -160,46 +141,6 @@ def check_dup():
     exists = bool(db.users.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
-
-# @app.route('/update_profile', methods=['POST'])
-# def save_img():
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         # 프로필 업데이트
-#         return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
-#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-#         return redirect(url_for("home"))
-
-# @app.route('/posting', methods=['POST'])
-# def posting():
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         # 포스팅하기
-#         return jsonify({"result": "success", 'msg': '포스팅 성공'})
-#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-#         return redirect(url_for("home"))
-
-# @app.route("/get_posts", methods=['GET'])
-# def get_posts():
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         # 포스팅 목록 받아오기
-#         return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다."})
-#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-#         return redirect(url_for("home"))
-
-# @app.route('/update_like', methods=['POST'])
-# def update_like():
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         # 좋아요 수 변경
-#         return jsonify({"result": "success", 'msg': 'updated'})
-#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-#         return redirect(url_for("home"))
 
 # 준호님 code end
 
@@ -305,13 +246,6 @@ def challenge_check():
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
-
-
-@app.route('/challenge/get', methods=['GET'])
-def challenge_get():
-    challenge_receive = request.args.get('challenge_give')
-    challenges = list(db.join.find({'join_challenge': challenge_receive}, {"_id": False}))
-    return jsonify({'all_challenges': challenges})
 
 
 # 수빈님 code end
